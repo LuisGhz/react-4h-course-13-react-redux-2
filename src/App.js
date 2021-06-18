d42ca98e5fc8da763d733023b961c58a604c588e
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-17 19:47:11 -0500

    Import useSelector, useDispatch and actions Dispatch counter actions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from 'actions';
 
 function App() {
+
+  const counter = useSelector(state => state.counter);
+  const login = useSelector(state => state.login);
+
+  const dispatch = useDispatch();
+
   return (
     <div>
+      <h1>Counter { counter }</h1>
+      <button onClick={ () => dispatch(increment()) }>+</button>
+      <button onClick={ () => dispatch(decrement()) } >-</button>
     </div>
   );
 }
